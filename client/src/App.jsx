8857ac130796845106536c043c4576c84ff6ae9d
Aryan-Tak <imaryan7781@gmail.com>
2024-07-12 23:02:43 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='p-1 bg-yellow-200'>Test</h1>
+    </>
+  )
+}
+
+export default App
